37a1e909bbfb204657447cf8c6aaefb9d27a9c07
Prashant P Shah <pshah.mumbai@gmail.com>
2010-11-04 20:48:19 +0530

    Changed main menu item from Home to Dashboard
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/views/template/menu.php
+++ system/application/views/template/menu.php
@@ -1,5 +1,5 @@
 		<ul id="nav">
-			<li><a href="#">Home</a></li>
+			<li><a href="#">Dashboard</a></li>
 			<li><a href="#">Accounts</a></li>
 			<li><a href="#">Vouchers</a></li>
 			<li><a href="#">Reports</a></li>
