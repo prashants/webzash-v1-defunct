f9e6a47fb6fa5cff7847533b2521be7cf7eb6c22
Prashant P Shah <pshah.mumbai@gmail.com>
2010-11-05 18:59:03 +0530

    Added form helper to autoload
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/config/autoload.php
+++ system/application/config/autoload.php
@@ -39,7 +39,7 @@
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database', 'session');
+$autoload['libraries'] = array('database', 'session', 'form_validation');
 
 
 /*
@@ -51,7 +51,7 @@ $autoload['libraries'] = array('database', 'session');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'path', 'html');
+$autoload['helper'] = array('url', 'path', 'html', 'form');
 
 
 /*
