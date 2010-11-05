945ec413edbd3ba8ad8bce198e2031a1037b4c6d
Prashant P Shah <pshah.mumbai@gmail.com>
2010-11-05 14:49:50 +0530

    Table based Chart of accounts
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/controllers/account.php
+++ system/application/controllers/account.php
@@ -1,10 +1,14 @@
 <?php
 class Account extends Controller {
 	protected $tree = "";
+	protected $tree_counter = 0;
 	function index()
 	{
 		$this->_get_groups(0);
-		$data['tree'] = $this->tree;
+		$data['tree'] = "<table border=0 cellpadding=5>";
+		$data['tree'] .= "<thead><tr><th>Name</th><th>Type</th><th colspan=2>Actions</th></tr></thead>";
+		$data['tree'] .= "<tbody>" . $this->tree . "</tbody>";
+		$data['tree'] .= "</table>";
 		$page_data['page_title'] = "Chart of accounts";
 		$page_data['page_links'] = array('group/new' => 'New Group', 'ledger/new' => 'New Ledger');
 		$this->load->view('template/header', $page_data);
@@ -14,33 +18,54 @@ class Account extends Controller {
 
 	function _get_groups($group_id)
 	{
-		$this->tree .= "<ul class=\"group-head";
-		$this->tree .= (in_array($group_id, array(0, 1, 2, 3, 4))) ? " group-first" : "";
-		$this->tree .= "\">";
 		if ($group_id > 0)
 		{
 			$group_q = $this->db->query('SELECT * FROM groups WHERE id = ?', array($group_id));
 			$group = $group_q->row();
-			$this->tree .= "<li class=\"group-item\">" . $group->name;
+			$this->tree .= "<tr class=\"group-tr\">";
+			$this->tree .= "<td class=\"group-td\">";
+			$this->tree .= $this->_add_tree_margin($this->tree_counter);
+			$this->tree .= $group->name;
+			$this->tree .= "</td>";
+			$this->tree .= "<td>GROUP</td>";
+			$this->tree .= "<td>" . anchor('group/edit/' . $group_id , img(array('src' => asset_url() . "/images/icons/edit.png", 'border' => '0', 'alt' => 'Edit group'))) . "</td>";
+			$this->tree .= "<td>" . anchor('group/delete' . $group_id, img(array('src' => asset_url() . "/images/icons/delete.png", 'border' => '0', 'alt' => 'Delete group'))) . "</td>";
+			$this->tree .= "</tr>";
 		}
 		$child_group_q = $this->db->query('SELECT * FROM groups WHERE parent_id = ?', array($group_id));
 		foreach ($child_group_q->result() as $row)
 		{
+			$this->tree_counter++;
 			$this->_get_groups($row->id);
+			$this->tree_counter--;
 		}
 		$ledger_q = $this->db->query('SELECT * FROM ledgers WHERE group_id = ?', array($group_id));
 		if ($ledger_q->num_rows() > 0)
 		{
-			$this->tree .= "<ul class=\"ledger-head\">";
+			$this->tree_counter++;
 			foreach ($ledger_q->result() as $row)
 			{
-				$this->tree .= "<li class=\"ledger-item\">";
+				$this->tree .= "<tr class=\"ledger-tr\">";
+				$this->tree .= "<td class=\"ledger-td\">";
+				$this->tree .= $this->_add_tree_margin($this->tree_counter);
 				$this->tree .= $row->name;
-				$this->tree .= "</li>";
+				$this->tree .= "</td>";
+				$this->tree .= "<td>LEDGER</td>";
+			$this->tree .= "<td>" . anchor('ledger/edit/' . $row->id, img(array('src' => asset_url() . "/images/icons/edit.png", 'border' => '0', 'alt' => 'Edit ledger'))) . "</td>";
+			$this->tree .= "<td>" . anchor('ledger/delete/' . $row->id, img(array('src' => asset_url() . "/images/icons/delete.png", 'border' => '0', 'alt' => 'Delete ledger'))) . "</td>";
+				$this->tree .= "</tr>";
 			}
-			$this->tree .= "</ul>";
+			$this->tree_counter--;
 		}
-		$this->tree .= "</li>";
-		$this->tree .= "</ul>";
+	}
+
+	function _add_tree_margin($counter)
+	{
+		$html = "";
+		for ($i = 2; $i <= $counter; $i++)
+		{
+			$html .= "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+		}
+		return $html;
 	}
 }
