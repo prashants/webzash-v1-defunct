bac3e64d734584d9d9c67444c63934dd9b97cffc
Prashant P Shah <pshah.mumbai@gmail.com>
2010-11-05 12:03:22 +0530

    Added database settings
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/config/database.php
+++ system/application/config/database.php
@@ -38,9 +38,9 @@ $active_group = "default";
 $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
-$db['default']['username'] = "";
+$db['default']['username'] = "root";
 $db['default']['password'] = "";
-$db['default']['database'] = "";
+$db['default']['database'] = "wz_webzash";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
 $db['default']['pconnect'] = TRUE;
