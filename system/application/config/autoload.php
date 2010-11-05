205d7b6b783c7a36752d43a89a79d0ad92b17e3a
Prashant P Shah <pshah.mumbai@gmail.com>
2010-11-05 14:49:10 +0530

    Load html helper
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/config/autoload.php
+++ system/application/config/autoload.php
@@ -51,7 +51,7 @@ $autoload['libraries'] = array('database', 'session');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'path');
+$autoload['helper'] = array('url', 'path', 'html');
 
 
 /*
