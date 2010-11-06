34bc1245cdc951f6620029be188e5644441dc492
Prashant P Shah <pshah.mumbai@gmail.com>
2010-11-06 11:32:26 +0530

    Added group edit action
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/controllers/group.php
+++ system/application/controllers/group.php
@@ -34,6 +34,7 @@ class Group extends Controller {
 		{
 			$this->load->view('template/header', $page_data);
 			$this->load->view('group/add', $data);
+			$this->load->view('template/footer');
 		}
 		else
 		{
@@ -49,6 +50,84 @@ class Group extends Controller {
 				$this->load->view('template/footer');
 			} else {
 				$this->session->set_flashdata('message', "Account group added successfully");
+				redirect('account');
+			}
+		}
+		return;
+	}
+
+	function edit($id)
+	{
+		$page_data['page_title'] = "Edit Group";
+		$id = $this->input->xss_clean($id);
+		$id = (int)$id;
+		if ($id < 1) {
+			$this->session->set_flashdata('error', "Invalid Account Group");
+			redirect('account');
+			return;
+		}
+		if ($id < 5) {
+			$this->session->set_flashdata('error', "Cannot edit system created Account Group");
+			redirect('account');
+			return;
+		}
+		$group_data_q = $this->db->query("SELECT * FROM groups WHERE id = ?", array($id));
+		if ($group_data_q->num_rows() < 1)
+		{
+			$this->session->set_flashdata('error', "Invalid Account Group");
+			redirect('account');
+			return;
+		}
+
+		$group_data = $group_data_q->row();
+
+		/* Form fields */
+		$data['group_name'] = array(
+			'name' => 'group_name',
+			'id' => 'group_name',
+			'maxlength' => '100',
+			'size' => '40',
+			'value' => $group_data->name,
+		);
+		$options = array();
+		$group_parent_q = $this->db->query('SELECT * FROM groups WHERE id > 0 AND id != ? ORDER BY name', array($id));
+		foreach ($group_parent_q->result() as $row)
+		{
+			$options[$row->id] = $row->name;
+		}
+		$data['group_parent'] = $options;
+		$data['group_parent_active'] = $group_data->parent_id;
+		$data['group_id'] = $id;
+
+		/* Form validations */
+		$this->form_validation->set_rules('group_name', 'Group name', 'trim|required|min_length[2]|max_length[100]|uniquewithid[groups.name.' . $id . ']');
+		$this->form_validation->set_rules('group_parent', 'Parent group', 'trim|required|is_natural_no_zero');
+
+		if ($this->form_validation->run() == FALSE)
+		{
+			if ($this->input->post('submit', TRUE))
+			{
+				$data['group_name']['value'] = $this->input->post('group_name', TRUE);
+				$data['group_parent_active'] = $this->input->post('group_parent', TRUE);
+			}
+			$this->load->view('template/header', $page_data);
+			$this->load->view('group/edit', $data);
+			$this->load->view('template/footer');
+		}
+		else
+		{
+			$data_name = $this->input->post('group_name', TRUE);
+			$data_parent_id = $this->input->post('group_parent', TRUE);
+			$data_id = $id;
+
+			if ( ! $this->db->query("UPDATE groups SET name = ?, parent_id = ? WHERE id = ?", array($data_name, $data_parent_id, $data_id)))
+			{
+				$this->session->set_flashdata('error', "Error updating Account group");
+				$this->load->view('template/header', $page_data);
+				$this->load->view('group/edit', $data);
+				$this->load->view('template/footer');
+			} else {
+				$this->session->set_flashdata('message', "Account group updated successfully");
 				redirect('account');
 			}
 		}
--- system/application/libraries/MY_Form_validation.php
+++ system/application/libraries/MY_Form_validation.php
@@ -20,9 +20,22 @@ class MY_Form_validation extends CI_Form_validation {
 		$CI =& get_instance();
 		list($table, $column) = explode('.', $field, 2);
 
-		$CI->form_validation->set_message('unique', 'The %s that you requested is already in use.');
+		$CI->form_validation->set_message('unique', 'The %s that you requested is already in use');
 
 		$query = $CI->db->query("SELECT COUNT(*) AS dupe FROM $table WHERE $column = '$str'");
+		$row = $query->row();
+		return ($row->dupe > 0) ? FALSE : TRUE;
+		echo "hello";
+	}
+
+	function uniquewithid($str, $field)
+	{
+		$CI =& get_instance();
+		list($table, $column, $id) = explode('.', $field, 3);
+
+		$CI->form_validation->set_message('uniquewithid', 'The %s that you requested is already in use');
+
+		$query = $CI->db->query("SELECT COUNT(*) AS dupe FROM $table WHERE $column = '$str' AND id != ?", array($id));
 		$row = $query->row();
 		return ($row->dupe > 0) ? FALSE : TRUE;
 		echo "hello";
