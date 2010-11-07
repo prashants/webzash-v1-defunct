57e2a212bb05c85c9e2ba5c6aafa738be1618327
Prashant P Shah <pshah.mumbai@gmail.com>
2010-11-07 15:12:38 +0530

    Custom date helpers
    Helps to convert date from and to mysql and php
    
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

---
+++ system/application/helpers/MY_date_helper.php
@@ -0,0 +1,18 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+if ( ! function_exists('date_php_to_mysql'))
+{
+	function date_php_to_mysql($dt)
+	{
+		// the helper function doesn't have access to $this, so we need to get a reference to the
+		// CodeIgniter instance.  We'll store that reference as $CI and use it instead of $this
+		$CI =& get_instance();
+
+		list($d, $m, $y) = explode('/', $dt);
+		$ts = mktime(0, 0, 0, $m, $d, $y);
+		return date('Y-m-d H:i:s', $ts);
+	}
+}
+
+/* End of file date_helper.php */
+/* Location: ./system/application/helpers/date_helper.php */
