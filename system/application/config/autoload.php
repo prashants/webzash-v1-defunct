313e0641bf7aef29b7e9641d103b240979f5f105
Prashant P Shah <pshah.mumbai@gmail.com>
2010-11-07 21:22:20 +0530

    Added Template library
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/config/autoload.php
+++ system/application/config/autoload.php
@@ -39,7 +39,7 @@
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database', 'session', 'form_validation', 'messages');
+$autoload['libraries'] = array('database', 'session', 'form_validation', 'messages', 'template');
 
 
 /*
