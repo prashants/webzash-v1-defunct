e3617906c95239ee0dd5be22d8753ee69b411b93
Prashant P Shah <pshah.mumbai@gmail.com>
2010-11-08 22:30:05 +0530

    Added get_ledger_balance function to Ledger model
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/models/ledger_model.php
+++ system/application/models/ledger_model.php
@@ -18,4 +18,26 @@ class Ledger_model extends Model {
 		}
 		return $options;
 	}
+
+	function get_ledger_balance($ledger_id)
+	{
+		$op_bal_q = $this->db->query('SELECT * FROM ledgers WHERE id = ?', $ledger_id);
+		$op_bal_r = $op_bal_q->row();
+		$op_bal = $op_bal_r->op_balance;
+		$op_bal_type = $op_bal_r->op_balance_dc;
+
+		if ($op_bal_type == "C")
+			$op_bal = $op_bal * -1;
+
+		$dr_total_q = $this->db->query('SELECT SUM(amount) AS drtotal FROM voucher_items join vouchers on  vouchers.id = voucher_items.voucher_id WHERE voucher_items.ledger_id = ? AND vouchers.draft = 0 AND voucher_items.dc = "D"', $ledger_id);
+		$dr_total_r = $dr_total_q->row();
+		$dr_total = $dr_total_r->drtotal;
+
+		$cr_total_q = $this->db->query('SELECT SUM(amount) AS drtotal FROM voucher_items join vouchers on  vouchers.id = voucher_items.voucher_id WHERE voucher_items.ledger_id = ? AND vouchers.draft = 0 AND voucher_items.dc = "C"', $ledger_id);
+		$cr_total_r = $cr_total_q->row();
+		$cr_total = $cr_total_r->drtotal;
+
+		$total = $op_bal + $dr_total - $cr_total;
+		return $total;
+	}
 }
