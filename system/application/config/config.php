536bdedb1c8e12ab272c695fdcc58a5e184ee989
Prashant P Shah <pshah.mumbai@gmail.com>
2010-11-12 16:27:43 +0530

    Remove index.php from URL
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/config/config.php
+++ system/application/config/config.php
@@ -23,7 +23,7 @@ $config['base_url']	= "http://localhost/webzash/";
 | variable so that it is blank.
 |
 */
-$config['index_page'] = "index.php";
+$config['index_page'] = "";
 
 /*
 |--------------------------------------------------------------------------
