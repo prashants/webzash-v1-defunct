dbd1f22a08eaa3d751a9f835c92ded1dde128ae1
Prashant P Shah <pshah.mumbai@gmail.com>
2010-11-14 15:38:17 +0530

    Updated date helper
    Removed need to get the current instance
    Added todays date in php format
    
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/helpers/MY_date_helper.php
+++ system/application/helpers/MY_date_helper.php
@@ -4,10 +4,6 @@ if ( ! function_exists('date_php_to_mysql'))
 {
 	function date_php_to_mysql($dt)
 	{
-		// the helper function doesn't have access to $this, so we need to get a reference to the
-		// CodeIgniter instance.  We'll store that reference as $CI and use it instead of $this
-		$CI =& get_instance();
-
 		list($d, $m, $y) = explode('/', $dt);
 		$ts = mktime(0, 0, 0, $m, $d, $y);
 		return date('Y-m-d H:i:s', $ts);
@@ -15,11 +11,13 @@ if ( ! function_exists('date_php_to_mysql'))
 
 	function date_mysql_to_php($dt)
 	{
-		// the helper function doesn't have access to $this, so we need to get a reference to the
-		// CodeIgniter instance.  We'll store that reference as $CI and use it instead of $this
-		$CI =& get_instance();
 		$ts = human_to_unix($dt);
 		return date('d/m/Y', $ts);
+	}
+
+	function date_today_php()
+	{
+		return date('d/m/Y');
 	}
 }
 
