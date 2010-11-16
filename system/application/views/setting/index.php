643ea133bbaf80c1c77b783a39bbeb286a81860e
Prashant P Shah <pshah.mumbai@gmail.com>
2010-11-16 22:03:55 +0530

    Added Print settings
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/views/setting/index.php
+++ system/application/views/setting/index.php
@@ -1,12 +1,21 @@
 <div>
 	<div id="left-col">
-		<div class="settings-title">
-			<?php echo anchor('setting/account', 'Account Settings', 'Account Settings'); ?>
+		<div class="settings-container">
+			<div class="settings-title">
+				<?php echo anchor('setting/account', 'Account Settings', array('title' => 'Account Settings')); ?>
+			</div>
+			<div class="settings-desc">
+				Setup account details, currency, time, etc.
+			</div>
 		</div>
-		<div class="settings-desc">
-			Setup account details, currency, time.
+		<div class="settings-container">
+			<div class="settings-title">
+				<?php echo anchor('setting', 'Print Settings', array('title' => 'Print Settings')); ?>
+			</div>
+			<div class="settings-desc">
+				Setup printing options for vouchers, reports, etc.
+			</div>
 		</div>
-
 	</div>
 	<div id="right-col">
 
