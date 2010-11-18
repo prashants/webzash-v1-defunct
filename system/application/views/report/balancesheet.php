9e58508a443a4e4b7cefc72a58f0e0f408701ee3
Prashant P Shah <pshah.mumbai@gmail.com>
2010-11-18 21:12:17 +0530

    Sample Balance sheet
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/views/report/balancesheet.php
+++ system/application/views/report/balancesheet.php
@@ -1,27 +1,85 @@
 <?php
 	$this->load->library('accountlist');
 
-	echo "<table>";
+	echo "<table border=0>";
 	echo "<tr valign=\"top\">";
-
-	$asset = new Accountlist();
-	echo "<td>";
-	$asset->init(1);
-	echo "<table border=0 cellpadding=5 class=\"generaltable\">";
-	echo "<thead><tr><th>Assets</th><th>Amount</th></tr></thead>";
-	$asset->account_st_short(0);
-	echo "</table>";
-	echo "</td>";
 
 	$liability = new Accountlist();
 	echo "<td>";
 	$liability->init(2);
-	echo "<table border=0 cellpadding=5 class=\"generaltable\">";
+	echo "<table border=0 cellpadding=5 class=\"generaltable\" width=\"450\">";
 	echo "<thead><tr><th>Liabilities</th><th>Amount</th></tr></thead>";
 	$liability->account_st_short(0);
 	echo "</table>";
 	echo "</td>";
 
+	$asset = new Accountlist();
+	echo "<td>";
+	$asset->init(1);
+	echo "<table border=0 cellpadding=5 class=\"generaltable\" width=\"450\">";
+	echo "<thead><tr><th>Assets</th><th>Amount</th></tr></thead>";
+	$asset->account_st_short(0);
+	echo "</table>";
+	echo "</td>";
+
 	echo "</tr>";
+
+	$income = new Accountlist();
+	$income->init(3);
+	$expense = new Accountlist();
+	$expense->init(4);
+	$pandl = $income->total - $expense->total;
+
+	echo "<tr>";
+	echo "<td>";
+	echo "<table border=0 cellpadding=5 class=\"vouchertable\" width=\"450\">";
+	echo "<tr valign=\"top\">";
+	echo "<td class=\"bold\">LIABILITY TOTAL</td>";
+	echo "<td align=\"right\" class=\"bold\">" . $liability->total . "</td>";
+	if ($pandl < 0)
+	{
+		echo "<tr valign=\"top\">";
+		echo "<td class=\"bold\">LOSS B/F</td>";
+		echo "<td align=\"right\" class=\"bold\">" . -$pandl . "</td>";
+		echo "</tr>";
+	} else {
+		echo "<tr>";
+		echo "<td>&nbsp;</td>";
+		echo "<td>&nbsp;</td>";
+		echo "</tr>";
+
+	}
+	echo "<tr valign=\"top\">";
+	echo "<td class=\"bold\">TOTAL</td>";
+	echo "<td align=\"right\" class=\"bold\">" . ($liability->total + $expense->total) . "</td>";
+	echo "</tr>";
+	echo "</table>";
+	echo "</td>";
+
+	echo "<td>";
+	echo "<table border=0 cellpadding=5 class=\"vouchertable\" width=\"450\">";
+	echo "<tr valign=\"top\">";
+	echo "<td class=\"bold\">ASSET TOTAL</td>";
+	echo "<td align=\"right\" class=\"bold\">" . $asset->total . "</td>";
+	echo "</tr>";
+	if ($pandl > 0)
+	{
+		echo "<tr valign=\"top\">";
+		echo "<td class=\"bold\">PROFIT B/F</td>";
+		echo "<td align=\"right\" class=\"bold\">" . $pandl . "</td>";
+		echo "</tr>";
+	} else {
+		echo "<tr>";
+		echo "<td>&nbsp;</td>";
+		echo "<td>&nbsp;</td>";
+		echo "</tr>";
+
+	}
+	echo "<tr valign=\"top\">";
+	echo "<td class=\"bold\">TOTAL</td>";
+	echo "<td align=\"right\" class=\"bold\">" . ($asset->total + $income->total) . "</td>";
+	echo "</tr>";
+	echo "</table>";
+	echo "</td>";
 	echo "</table>";
 ?>
