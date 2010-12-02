b59f3f6603ff517eff2202e0af8a0f189aa43a60
Prashant P Shah <pshah.mumbai@gmail.com>
2010-12-02 19:45:22 +0530

    Added Label to account list
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/views/admin/active.php
+++ system/application/views/admin/active.php
@@ -1,14 +1,16 @@
 <?php
-echo form_open('admin/active');
+	echo form_open('admin/active');
 
-echo "<p>";
-echo form_dropdown('accounts', $accounts);
-echo "</p>";
+	echo "<p>";
+	echo "Currently available accounts";
+	echo "<br />";
+	echo form_dropdown('accounts', $accounts);
+	echo "</p>";
 
-echo "<p>";
-echo form_submit('submit', 'Change');
-echo " ";
-echo anchor('admin', 'Back', array('title' => 'Back to admin'));
-echo form_close();
-echo "</p>";
+	echo "<p>";
+	echo form_submit('submit', 'Change');
+	echo " ";
+	echo anchor('admin', 'Back', array('title' => 'Back to admin'));
+	echo form_close();
+	echo "</p>";
 ?>
