5323b4c66af52ab6dc5eaee82ca65d5bc55e26d2
Prashant P Shah <pshah.mumbai@gmail.com>
2010-12-08 11:38:46 +0530

    Tag list now uses the simple table style
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/views/tag/index.php
+++ system/application/views/tag/index.php
@@ -1,7 +1,7 @@
 <?php
 	$tags_q = $this->db->get("tags");
-	echo "<table border=0 cellpadding=5 class=\"generaltable\">";
-	echo "<thead><tr><th>Title</th><th>Color</th><th colspan=5>Actions</th></tr></thead>";
+	echo "<table border=0 cellpadding=5 class=\"simple-table tag-table\">";
+	echo "<thead><tr><th>Title</th><th>Color</th><th colspan=5></th></tr></thead>";
 	echo "<tbody>";
 	$odd_even = "odd";
 	foreach ($tags_q->result() as $row)
@@ -10,9 +10,9 @@
 		echo "<td>" . $row->title . "</td>";
 		echo "<td>" . $this->Tag_model->show_voucher_tag($row->id) . "</td>";
 
-		echo "<td>" . anchor('tag/edit/' . $row->id , img(array('src' => asset_url() . "images/icons/edit.png", 'border' => '0', 'alt' => 'Edit Tag')), array('title' => 'Edit Tag')) . "</td>";
-
-		echo "<td>" . anchor('tag/delete/' . $row->id , img(array('src' => asset_url() . "images/icons/delete.png", 'border' => '0', 'alt' => 'Delete Tag', 'class' => "confirmClick", 'title' => "Delete Tag")), array('title' => 'Delete  Tag')) . "</td>";
+		echo "<td>" . anchor('tag/edit/' . $row->id , "Edit", array('title' => 'Edit Tag', 'class' => 'red-link'));
+		echo " &nbsp;";
+		echo anchor('tag/delete/' . $row->id , img(array('src' => asset_url() . "images/icons/delete.png", 'border' => '0', 'alt' => 'Delete Tag', 'class' => "confirmClick", 'title' => "Delete Tag")), array('title' => 'Delete  Tag')) . "</td>";
 		echo "</tr>";
 		$odd_even = ($odd_even == "odd") ? "even" : "odd";
 	}
