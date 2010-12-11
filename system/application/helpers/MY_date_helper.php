054e55cfe9a6c3f7ad2f54683beeb1cab2152770
Prashant P Shah <pshah.mumbai@gmail.com>
2010-12-11 12:29:39 +0530

    Updated date helper to support the date formats
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/helpers/MY_date_helper.php
+++ system/application/helpers/MY_date_helper.php
@@ -4,20 +4,77 @@ if ( ! function_exists('date_php_to_mysql'))
 {
 	function date_php_to_mysql($dt)
 	{
-		list($d, $m, $y) = explode('/', $dt);
+		$CI =& get_instance();
+		$current_date_format = $CI->config->item('account_date_format');
+		list($d, $m, $y) = array(0, 0, 0);
+		switch ($current_date_format)
+		{
+		case 'dd/mm/yyyy':
+			list($d, $m, $y) = explode('/', $dt);
+			break;
+		case 'mm/dd/yyyy':
+			list($m, $d, $y) = explode('/', $dt);
+			break;
+		case 'yyyy/mm/dd':
+			list($y, $m, $d) = explode('/', $dt);
+			break;
+		default:
+			$CI->messages->add('Invalid date format. Please check your account settings', 'error');
+			return "";
+		}
 		$ts = mktime(0, 0, 0, $m, $d, $y);
 		return date('Y-m-d H:i:s', $ts);
 	}
+}
 
+if ( ! function_exists('date_mysql_to_php'))
+{
 	function date_mysql_to_php($dt)
 	{
 		$ts = human_to_unix($dt);
-		return date('d/m/Y', $ts);
+		$CI =& get_instance();
+		$current_date_format = $CI->config->item('account_date_format');
+		switch ($current_date_format)
+		{
+		case 'dd/mm/yyyy':
+			return date('d/m/Y', $ts);
+			break;
+		case 'mm/dd/yyyy':
+			return date('m/d/Y', $ts);
+			break;
+		case 'yyyy/mm/dd':
+			return date('Y/m/d', $ts);
+			break;
+		default:
+			$CI->messages->add('Invalid date format. Please check your account settings', 'error');
+			return "";
+		}
+		return;
 	}
+}
 
+if ( ! function_exists('date_today_php'))
+{
 	function date_today_php()
 	{
-		return date('d/m/Y');
+		$CI =& get_instance();
+		$current_date_format = $CI->config->item('account_date_format');
+		switch ($current_date_format)
+		{
+		case 'dd/mm/yyyy':
+			return date('d/m/Y');
+			break;
+		case 'mm/dd/yyyy':
+			return date('m/d/Y');
+			break;
+		case 'yyyy/mm/dd':
+			return date('Y/m/d');
+			break;
+		default:
+			$CI->messages->add('Invalid date format. Please check your account settings', 'error');
+			return "";
+		}
+		return;
 	}
 }
 
