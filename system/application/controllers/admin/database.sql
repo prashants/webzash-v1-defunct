f175fcc0ef7682c9ff4e816fb4fcd7e681f41dfb
Prashant P Shah <pshah.mumbai@gmail.com>
2010-12-15 11:39:15 +0530

    Added user column to logs
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/controllers/admin/database.sql
+++ system/application/controllers/admin/database.sql
@@ -85,6 +85,7 @@ CREATE TABLE IF NOT EXISTS logs (
   date datetime NOT NULL,
   level int(1) NOT NULL,
   host_ip varchar(25) NOT NULL,
+  user varchar(25) NOT NULL,
   url varchar(255) NOT NULL,
   user_agent varchar(100) NOT NULL,
   message_title varchar(255) NOT NULL,
