4ec0a44db39763c12e18eb10daf05d20ac46e221
Prashant P Shah <pshah.mumbai@gmail.com>
2011-01-04 21:06:20 +0530

    Added change active account action
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/controllers/user.php
+++ system/application/controllers/user.php
@@ -84,6 +84,113 @@ class User extends Controller {
 		$this->messages->add('Logged out.', 'success');
 		redirect('user/login');
 	}
+
+	function account()
+	{
+		$this->template->set('page_title', 'Change Account');
+
+		/* Check access */
+		if ( ! ($this->session->userdata('user_name')))
+		{
+			$this->messages->add('Permission denied', 'error');
+			redirect('');
+			return;
+		}
+
+		/* Currently active account */
+		$data['active_account'] = $this->session->userdata('db_active_label');
+
+		/* Getting list of files in the config - accounts directory */
+		$accounts_list = get_filenames($this->config->item('config_path') . 'accounts');
+		$data['accounts'] = array();
+		if ($accounts_list)
+		{
+			foreach ($accounts_list as $row)
+			{
+				/* Only include file ending with .ini */
+				if (substr($row, -4) == ".ini")
+				{
+					$ini_label = substr($row, 0, -4);
+					$data['accounts'][$ini_label] = $ini_label;
+				}
+			}
+		}
+
+		/* Form validations */
+		$this->form_validation->set_rules('account', 'Account', 'trim|required');
+
+		/* Repopulating form */
+		if ($_POST)
+		{
+			$data['active_account'] = $this->input->post('account', TRUE);
+		}
+
+		/* Validating form : only if label name is not set from URL */
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->messages->add(validation_errors(), 'error');
+			$this->template->load('user_template', 'user/account', $data);
+			return;
+		} else {
+			$data_active_account = $this->input->post('account', TRUE);
+			$ini_file = $this->config->item('config_path') . "accounts/" . $data_active_account . ".ini";
+
+			/* Check if database ini file exists */
+			if ( ! get_file_info($ini_file))
+			{
+				$this->messages->add('Account settings file is missing.', 'error');
+				$this->template->load('user_template', 'user/account', $data);
+				return;
+			}
+
+			/* Parsing database ini file */
+			$current_account = parse_ini_file($ini_file);
+			if ( ! $current_account)
+			{
+				$this->messages->add('Invalid account settings file.', 'error');
+				$this->template->load('user_template', 'user/account', $data);
+				return;
+			}
+
+			/* Check if all needed variables are set in ini file */
+			if ( ! isset($current_account['db_hostname']))
+			{
+				$this->messages->add('Hostname missing from account settings file.', 'error');
+				$this->template->load('user_template', 'user/account', $data);
+				return;
+			}
+			if ( ! isset($current_account['db_port']))
+			{
+				$this->messages->add('Port missing from account settings file. Default MySQL port is 3306.', 'error');
+				$this->template->load('user_template', 'user/account', $data);
+				return;
+			}
+			if ( ! isset($current_account['db_name']))
+			{
+				$this->messages->add('Database name missing from account settings file.', 'error');
+				$this->template->load('user_template', 'user/account', $data);
+				return;
+			}
+			if ( ! isset($current_account['db_username']))
+			{
+				$this->messages->add('Database username missing from account settings file.', 'error');
+				$this->template->load('user_template', 'user/account', $data);
+				return;
+			}
+			if ( ! isset($current_account['db_password']))
+			{
+				$this->messages->add('Database password missing from account settings file.', 'error');
+				$this->template->load('user_template', 'user/account', $data);
+				return;
+			}
+
+			/* Setting new account database details in session */
+			$this->session->set_userdata('db_active_label', $data_active_account);
+			$this->messages->add('Active account changed.', 'success');
+			redirect('');
+		}
+		return;
+	}
 }
 
 /* End of file user.php */
---
+++ system/application/views/user/account.php
@@ -0,0 +1,23 @@
+<?php
+	echo form_open('user/account');
+
+	echo "<p>";
+	echo "<b>Currently active account : </b>";
+	$current_active_account = $this->session->userdata('db_active_label');
+	echo ($current_active_account) ? $current_active_account : "(None)";
+	echo "</p>";
+
+	echo "<p>";
+	echo "Select account";
+	echo "<br />";
+	echo form_dropdown('account', $accounts, $active_account);
+	echo "</p>";
+
+	echo "<p>";
+	echo form_submit('submit', 'Activate');
+	echo " ";
+	echo anchor('', 'Back', array('title' => 'Back to accounts'));
+	echo "</p>";
+
+	echo form_close();
+
