b21bdbd8010605b903eaf85f7b0936fefd665362
Prashant P Shah <pshah.mumbai@gmail.com>
2011-01-06 12:30:43 +0530

    Added Help for enabling or disabling logging
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/views/help/index.php
+++ system/application/views/help/index.php
@@ -1,4 +1,9 @@
 <ul id="qa-list">
+<li><span class="qa-heading">General</span>
+	<ul>
+	<li><a href="#general-1" class="anchor-link-a">How do I enable / disable logging ?</a></li>
+	</ul>
+</li>
 <li><span class="qa-heading">Printing</span>
 	<ul>
 	<li><a href="#print-1" class="anchor-link-a">How do I modify the voucher print format ?</a></li>
@@ -13,6 +18,12 @@
 </ul>
 
 <br /><br />
+
+<div class="qa-section" id="general-1">
+	<a name="print-1"></a>
+	<div class="qa-question">Q. How do I enable / disable logging ?</div>
+	<div class="qa-answer">You need "administrator" permissions to do this. After logging with "administrator" permissions click on "Administer" link on the top of the page. Then go to "General Settings" and check / uncheck the "Log Messages" option and click on "Update".<br /><br />Note: You can do this manually by opening the "config/settings/general.ini" in a text editor and changing the value of log = "1" or log = "0" to enable or disable logging respectively.</div>
+</div>
 
 <div class="qa-section" id="print-1">
 	<a name="print-1"></a>
