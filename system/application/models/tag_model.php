fd0033582f92de60dd55b99cdd6193149a36d01d
Prashant P Shah <pshah.mumbai@gmail.com>
2011-01-11 11:56:25 +0530

    Converted tag model to active records
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/models/tag_model.php
+++ system/application/models/tag_model.php
@@ -12,7 +12,8 @@ class Tag_model extends Model {
 		$options = array();
 		if ($allow_none)
 			$options[0] = "(None)";
-		$tag_q = $this->db->query('SELECT * FROM tags ORDER BY title ASC');
+		$this->db->from('tags')->order_by('title', 'asc');
+		$tag_q = $this->db->get();
 		foreach ($tag_q->result() as $row)
 		{
 			$options[$row->id] = $row->title;
@@ -24,35 +25,35 @@ class Tag_model extends Model {
 	{
 		if ($tag_id < 1)
 			return "";
-		$tag_q = $this->db->query("SELECT * FROM tags WHERE id = ?", array($tag_id));
+		$this->db->from('tags')->where('id', $tag_id)->limit(1);
+		$tag_q = $this->db->get();
 		if ($tag = $tag_q->row())
-		{
 			return "<span class=\"tags\" style=\"color:#" . $tag->color . "; background-color:#" . $tag->background . "\">" . $tag->title . "</span>";
-		}
-		return "";
+		else
+			return "";
 	}
 
 	function show_voucher_tag_link($tag_id)
 	{
 		if ($tag_id < 1)
 			return "";
-		$tag_q = $this->db->query("SELECT * FROM tags WHERE id = ?", array($tag_id));
+		$this->db->from('tags')->where('id', $tag_id)->limit(1);
+		$tag_q = $this->db->get();
 		if ($tag = $tag_q->row())
-		{
 			return "<span class=\"tags\" style=\"color:#" . $tag->color . "; background-color:#" . $tag->background . "\">" . anchor("voucher/show/tag/" . $tag->id , $tag->title, array('style' => 'text-decoration:none;color:#' . $tag->color . ';')) . "</span>";
-		}
-		return "";
+		else
+			return "";
 	}
 	
 	function tag_name($tag_id)
 	{
 		if ($tag_id < 1)
 			return "";
-		$tag_q = $this->db->query("SELECT * FROM tags WHERE id = ?", array($tag_id));
+		$this->db->from('tags')->where('id', $tag_id)->limit(1);
+		$tag_q = $this->db->get();
 		if ($tag = $tag_q->row())
-		{
 			return $tag->title;
-		}
-		return "";
+		else
+			return "";
 	}
 }
