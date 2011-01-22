b37f69c8c5923ebbdfde56acfbe198d5747514e7
Prashant P Shah <pshah.mumbai@gmail.com>
2011-01-22 19:48:30 +0530

    Updated footer website link to webzash.org
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/views/user_template.php
+++ system/application/views/user_template.php
@@ -119,7 +119,7 @@
 </div>
 <div id="footer">
 	<?php if (isset($page_footer)) echo $page_footer ?>
-	<a href="http://webzash.wordpress.com" target="_blank">Webzash<a/> is licensed under <a href="http://www.gnu.org/licenses/agpl-3.0.txt" target="_blank">GNU Affero General Public License, version 3</a> as published by the Free Software Foundation.
+	<a href="http://webzash.org" target="_blank">Webzash<a/> is licensed under <a href="http://www.gnu.org/licenses/agpl-3.0.txt" target="_blank">GNU Affero General Public License, version 3</a> as published by the Free Software Foundation.
 </div>
 </body>
 </html>
