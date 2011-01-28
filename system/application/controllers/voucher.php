0c955abab3781ba787247ffdea211566efdc5202
Prashant P Shah <pshah.mumbai@gmail.com>
2011-01-28 15:06:21 +0530

    Display all actions on voucher edit
        - Display Voucher View, Edit, Print, Email, Download, Reconciliation
        - The messages cannot be set in edit() method since the messages
          are too long and session is getting corrupted after redirect
        - Moved the updated voucher info to sessions and showing the message in
          in the show action
    
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/controllers/voucher.php
+++ system/application/controllers/voucher.php
@@ -155,6 +155,32 @@ class Voucher extends Controller {
 			$this->session->unset_userdata('voucher_added_number');
 		}
 
+		/* Show voucher edit actions */
+		if ($this->session->userdata('voucher_updated_show_action'))
+		{
+			$voucher_updated_id_temp = $this->session->userdata('voucher_updated_id');
+			$voucher_updated_type_temp = $this->session->userdata('voucher_updated_type');
+			$voucher_updated_number_temp = $this->session->userdata('voucher_updated_number');
+			$voucher_updated_message = 'Updated ' . ucfirst($voucher_updated_type_temp) . ' Voucher number ' . voucher_number_prefix($voucher_updated_type_temp) . $voucher_updated_number_temp . ".";
+			$voucher_updated_message .= " You can [ ";
+			$voucher_updated_message .= anchor('voucher/view/' . strtolower($voucher_updated_type_temp) . "/" . $voucher_updated_id_temp, 'View', array('class' => 'anchor-link-a')) . " | ";
+			$voucher_updated_message .= anchor('voucher/edit/' . strtolower($voucher_updated_type_temp) . "/" . $voucher_updated_id_temp, 'Edit', array('class' => 'anchor-link-a')) . " | ";
+			$voucher_updated_message .= anchor_popup('voucher/printpreview/' . strtolower($voucher_updated_type_temp) . "/" . $voucher_updated_id_temp , 'Print', array('class' => 'anchor-link-a', 'width' => '600', 'height' => '600')) . " | ";
+			$voucher_updated_message .= anchor_popup('voucher/email/' . strtolower($voucher_updated_type_temp) . "/" . $voucher_updated_id_temp, 'Email', array('class' => 'anchor-link-a', 'width' => '500', 'height' => '300')) . " | ";
+			$voucher_updated_message .= anchor('voucher/download/' . strtolower($voucher_updated_type_temp) . "/" . $voucher_updated_id_temp, 'Download', array('class' => 'anchor-link-a'));
+			$voucher_updated_message .= " ] it.";
+			$this->messages->add($voucher_updated_message, 'success');
+
+			if ($this->session->userdata('voucher_updated_has_reconciliation'))
+				$this->messages->add('Previous reconciliations for this voucher are no longer valid. You need to redo the reconciliations for this voucher.', 'success');
+
+			$this->session->unset_userdata('voucher_updated_show_action');
+			$this->session->unset_userdata('voucher_updated_id');
+			$this->session->unset_userdata('voucher_updated_type');
+			$this->session->unset_userdata('voucher_updated_number');
+			$this->session->unset_userdata('voucher_updated_has_reconciliation');
+		}
+
 		$html = "<table border=0 cellpadding=5 class=\"simple-table\">";
 		$html .= "<thead><tr><th>Date</th><th>No</th><th>Ledger A/C</th><th>Type</th><th>DR Amount</th><th>CR Amount</th><th></th></tr></thead>";
 		$html .= "<tbody>";
@@ -577,26 +603,6 @@ class Voucher extends Controller {
 			/* Success */
 			$this->db->trans_complete();
 
-			/* Check for Voucher Print, Download, Email */
-			if ($this->input->post('voucher_print', TRUE))
-			{
-				$this->session->set_userdata('print_voucher', TRUE);
-				$this->session->set_userdata('print_voucher_type', strtolower($voucher_type));
-				$this->session->set_userdata('print_voucher_id', $voucher_id);
-			}
-			if ($this->input->post('voucher_email', TRUE))
-			{
-				$this->session->set_userdata('email_voucher', TRUE);
-				$this->session->set_userdata('email_voucher_type', strtolower($voucher_type));
-				$this->session->set_userdata('email_voucher_id', $voucher_id);
-			}
-			if ($this->input->post('voucher_download', TRUE))
-			{
-				$this->session->set_userdata('download_voucher', TRUE);
-				$this->session->set_userdata('download_voucher_type', strtolower($voucher_type));
-				$this->session->set_userdata('download_voucher_id', $voucher_id);
-			}
-
 			$this->session->set_userdata('voucher_added_show_action', TRUE);
 			$this->session->set_userdata('voucher_added_id', $voucher_id);
 			$this->session->set_userdata('voucher_added_type', $voucher_type);
@@ -973,37 +979,17 @@ class Voucher extends Controller {
 			/* Success */
 			$this->db->trans_complete();
 
-			/* Check for Voucher Print, Download, Email */
-			if ($this->input->post('voucher_print', TRUE))
-			{
-				$this->session->set_userdata('print_voucher', TRUE);
-				$this->session->set_userdata('print_voucher_type', strtolower($voucher_type));
-				$this->session->set_userdata('print_voucher_id', $voucher_id);
-			}
-			if ($this->input->post('voucher_email', TRUE))
-			{
-				$this->session->set_userdata('email_voucher', TRUE);
-				$this->session->set_userdata('email_voucher_type', strtolower($voucher_type));
-				$this->session->set_userdata('email_voucher_id', $voucher_id);
-			}
-			if ($this->input->post('voucher_download', TRUE))
-			{
-				$this->session->set_userdata('download_voucher', TRUE);
-				$this->session->set_userdata('download_voucher_type', strtolower($voucher_type));
-				$this->session->set_userdata('download_voucher_id', $voucher_id);
-			}
-
-			/* Voucher Actions */
-			$voucher_success_links = "You can ";
-			$voucher_success_links .= anchor('voucher/view/' . strtolower($voucher_type) . "/" . $voucher_id, 'View', array('class' => 'anchor-link-a')) . " or ";
-			$voucher_success_links .= anchor('voucher/download/' . strtolower($voucher_type) . "/" . $voucher_id, 'Download', array('class' => 'anchor-link-a'));
-			$voucher_success_links .= " it.";
-
-			$this->messages->add('Updated ' . ucfirst($voucher_type) . ' Voucher number ' . voucher_number_prefix($voucher_type) . $data_number . ". " . $voucher_success_links, 'success');
-			$this->logger->write_message("success", "Updated " . ucfirst($voucher_type) . " Voucher number " . voucher_number_prefix($voucher_type) . $data_number . " [id:" . $voucher_id . "]");
-
+			$this->session->set_userdata('voucher_updated_show_action', TRUE);
+			$this->session->set_userdata('voucher_updated_id', $voucher_id);
+			$this->session->set_userdata('voucher_updated_type', $voucher_type);
+			$this->session->set_userdata('voucher_updated_number', $data_number);
 			if ($data_has_reconciliation)
-				$this->messages->add('Previous reconciliations for this voucher are no longer valid. You need to redo the reconciliations for this voucher.', 'success');
+				$this->session->set_userdata('voucher_updated_has_reconciliation', TRUE);
+			else
+				$this->session->set_userdata('voucher_updated_has_reconciliation', FALSE);
+
+			/* Showing success message in show() method since message is too long for storing it in session */
+			$this->logger->write_message("success", "Updated " . ucfirst($voucher_type) . " Voucher number " . voucher_number_prefix($voucher_type) . $data_number . " [id:" . $voucher_id . "]");
 
 			redirect('voucher/show/' . $voucher_type);
 			return;
