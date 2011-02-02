99111337bf22cd386070f7813a9f69436d5040f6
Prashant P Shah <pshah.mumbai@gmail.com>
2011-02-02 20:27:24 +0530

    Updated CHANGELOG.txt
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/config/config.php
+++ system/application/config/config.php
@@ -376,7 +376,7 @@ $config['backup_path'] = 'backups/';
 | Sets the current application version
 |
 */
-$config['application_version'] = '1.1';
+$config['application_version'] = '1.2';
 
 /* End of file config.php */
 /* Location: ./system/application/config/config.php */
