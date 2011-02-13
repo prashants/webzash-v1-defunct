989df69bd800ee93804668cffbc1a9c28c6af7cf
Prashant P Shah <pshah.mumbai@gmail.com>
2011-02-13 18:59:39 +0530

    Updated voucher types add/edit to show relevant options
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/views/setting/vouchertypes/add.php
+++ system/application/views/setting/vouchertypes/add.php
@@ -1,3 +1,22 @@
+<script type="text/javascript">
+$(document).ready(function() {
+	$('#voucher_type_base_type').change(function() {
+		if ($(this).val() == "1") {
+			$('#bank_cash_ledger_restriction').show();
+			$('#stock_voucher_type').hide();
+		} else if ($(this).val() == "2") {
+			$('#bank_cash_ledger_restriction').hide();
+			$('#stock_voucher_type').show();
+		} else {
+			$('#bank_cash_ledger_restriction').show();
+			$('#stock_voucher_type').show();
+		}
+	});
+	/* initialize */
+	$('#voucher_type_base_type').trigger('change');
+});
+</script>
+
 <?php
 	echo form_open('setting/vouchertypes/add');
 
@@ -24,16 +43,16 @@
 	echo "<p>";
 	echo form_label('Base Type', 'voucher_type_base_type');
 	echo "<br />";
-	echo form_dropdown('voucher_type_base_type', $voucher_type_base_types, $voucher_type_base_type_active);
+	echo form_dropdown('voucher_type_base_type', $voucher_type_base_types, $voucher_type_base_type_active, 'id="voucher_type_base_type"');
 	echo "</p>";
 
-	echo "<p>";
+	echo "<p id=\"bank_cash_ledger_restriction\">";
 	echo form_label('Restrictions', 'bank_cash_ledger_restriction');
 	echo "<br />";
 	echo form_dropdown('bank_cash_ledger_restriction', $bank_cash_ledger_restrictions, $bank_cash_ledger_restriction_active);
 	echo "</p>";
 
-	echo "<p>";
+	echo "<p id=\"stock_voucher_type\">";
 	echo form_label('Stock Voucher Type', 'stock_voucher_type');
 	echo "<br />";
 	echo form_dropdown('stock_voucher_type', $stock_voucher_types, $stock_voucher_type_active);
