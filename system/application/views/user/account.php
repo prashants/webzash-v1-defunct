eb4a81cc42a7b52947b34ebd4843ac2f98dc55b1
Prashant P Shah <pshah.mumbai@gmail.com>
2012-01-29 19:27:41 +0530

    remove the sample.ini file
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- system/application/views/user/account.php
+++ system/application/views/user/account.php
@@ -7,17 +7,22 @@
 	echo ($current_active_account) ? $current_active_account : "(None)";
 	echo "</p>";
 
-	echo "<p>";
-	echo "Select account";
-	echo "<br />";
-	echo form_dropdown('account', $accounts, $active_account);
-	echo "</p>";
+	if ($accounts)
+	{
+		echo "<p>";
+		echo "Select account";
+		echo "<br />";
+		echo form_dropdown('account', $accounts, $active_account);
+		echo "</p>";
 
-	echo "<p>";
-	echo form_submit('submit', 'Activate');
-	echo " ";
-	echo anchor('', 'Back', array('title' => 'Back to accounts'));
-	echo "</p>";
+		echo "<p>";
+		echo form_submit('submit', 'Activate');
+		echo " ";
+		echo anchor('', 'Back', array('title' => 'Back to accounts'));
+		echo "</p>";
+	} else {
+		echo "<p>Please create a account by clicking on the above 'Create account' button.</p>";
+	}
 
 	echo form_close();
 
