4f1765e0859fccea31df8acc12ee4ab0a37b904a
manuthalasseril <manuthalasseril@yahoo.co.in>
2013-09-21 23:12:27 +0530

    Creating tree structure on chart of accounts
    Add a script function for tree structure

--- system/application/views/account/index.php
+++ system/application/views/account/index.php
@@ -1,3 +1,66 @@
+<style>
+    .tr-group{
+        display:none;
+    }
+    .tr-ledger{
+        display:none;
+    }
+    .td-group,.td-group label{
+        cursor:pointer;
+    }
+</style>
+<script type="text/javascript">
+    var mode=true;
+    var processTr = function(currElem,spaceCount,innerTrshow){
+        innerTrshow=innerTrshow||false;
+        var display='table-row';
+        if(mode==false)
+            display='none';
+        var nextnbspStr=currElem.nextSibling.children[0].innerHTML;
+        var nextnbspCount=((nextnbspStr.match(/&nbsp;/g) || []).length);
+        nextnbspCount=(nextnbspCount-1)/6;
+        if(innerTrshow==true){
+            var innertrnbspStr=currElem.children[0].innerHTML;
+            var innertrnbspCount=((innertrnbspStr.match(/&nbsp;/g) || []).length);
+            innertrnbspCount--;
+            if(innertrnbspCount>=6)
+                innertrnbspCount=innertrnbspCount/6;
+            spaceCount=innertrnbspCount;
+        }
+        if(spaceCount==nextnbspCount)
+            return;
+        else{
+            var innerSign;
+            if(spaceCount==(nextnbspCount-1)){
+                currElem.nextSibling.style.display = display;
+                innerSign=currElem.nextSibling.children[0].children[0].innerHTML;
+            }
+            if(innerSign=='-'){
+                    currElem.nextSibling.style.display = display;
+                    processTr(currElem.nextSibling,spaceCount,true);
+            }
+            processTr(currElem.nextSibling,spaceCount,false);
+        }
+    }
+    $(document).ready(function() { 
+        $('.tr-group').click(function(){
+            if((this.children[0].children[0].innerHTML)=='+'){
+                mode=true;
+                this.children[0].children[0].innerHTML='-';
+            }
+            else{
+                mode=false;
+                this.children[0].children[0].innerHTML='+';
+            }
+            var nbspStr=this.children[0].innerHTML;
+            var nbspCount = ((nbspStr.match(/&nbsp;/g) || []).length);
+            nbspCount--;
+            if(nbspCount>=6)
+                nbspCount=nbspCount/6;
+            processTr(this,nbspCount);                                                             
+        });
+    });
+</script>
 <?php
 	$this->load->library('accountlist');
 	echo "<table>";
